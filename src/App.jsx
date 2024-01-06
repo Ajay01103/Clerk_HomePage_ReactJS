e9126f86607a1f533d5db65a9d541c74b78708ba
Ajay01103 <ajaysingh01103@gmail.com>
2024-01-06 17:10:00 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import CTA from "./components/CTA"
 import CTA2 from "./components/CTA2"
+import CTA3 from "./components/CTA3"
 import Hero from "./components/Hero"
 import LogoGrid from "./components/LogoGrid"
 import { NavbarWithMegaMenu } from "./components/Navbar"
@@ -12,6 +13,7 @@ function App() {
       <LogoGrid />
       <CTA />
       <CTA2 />
+      {/* <CTA3 /> */}
       {/* <Features /> */}
     </>
   )
